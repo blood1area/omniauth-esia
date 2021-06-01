ce244860add0478d8bfd183813c13be903e58618
burakaev <d.burakaev@pmtonline.ru>
2021-06-01 13:33:40 +0300

    test

--- lib/omniauth/strategies/esia.rb
+++ lib/omniauth/strategies/esia.rb
@@ -89,7 +89,7 @@ module OmniAuth
         begin
           _tmp = File.read(file_path)
           _file_path_signed = "%s.sig" % file_path
-          system("/opt/cprocsp/bin/amd64/cryptcp -sign -thumbprint 'f7f6b0d88ce27181bbe2773b50f037016c144212' -strict %s %s" % [file_path, _file_path_signed])
+          system("/opt/cprocsp/bin/amd64/cryptcp -sign -thumbprint 'f7f6b0d88ce27181bbe2773b50f037016c144212' %s %s" % [file_path, _file_path_signed])
         ensure
           _signed_string = File.read(_file_path_signed).gsub(/\n/, "")
         end
